1cf2666e6b2fec09122c8ba732a375f726b46929
smbh <SmbHop@gmail.com>
2020-07-18 22:15:41 +0430

    update settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,10 +3,12 @@ from .base import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["api.mobogym.com", "45.156.185.155"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["api.mobogym.com", "103.215.221.145"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
